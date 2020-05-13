f67ff5dc3681a61ba937dc8eb63e42a1005d3c28
Zülkarneyn Yavaş <zulkarneyn.yavas@clockwork.com.tr>
2020-05-13 10:45:31 +0300

    Update holy_pop_up.php

--- upload/catalog/controller/extension/module/holy_pop_up.php
+++ upload/catalog/controller/extension/module/holy_pop_up.php
@@ -7,15 +7,20 @@ class ControllerExtensionModuleHolyPopUp extends Controller {
 
 		if ($setting && $setting['status']) {
 
-			$data = array();
-			
-			$data['image'] = $setting['image'];
-			$data['url'] = $setting['url'];
-			$data['bg_color'] = $setting['bg_color'];
-			$data['padding'] = $setting['padding'];
-			$data['token'] = $setting['token'];
+			if (($setting['date_start'] == '0000-00-00' || strtotime($setting['date_start']) < time()) && 
+				($setting['date_end'] == '0000-00-00' || strtotime($setting['date_end']) > time())) {
 
-			return $this->load->view('extension/module/holy_pop_up', $data);
+				$data = array();
+				
+				$data['image'] = $setting['image'];
+				$data['url'] = $setting['url'];
+				$data['bg_color'] = $setting['bg_color'];
+				$data['padding'] = $setting['padding'];
+				$data['token'] = $setting['token'];
+
+				return $this->load->view('extension/module/holy_pop_up', $data);
+
+			}
 		}
 	}
 }
